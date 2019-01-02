1ee7e7094225c13a1902fb2070440ea0adf95a31
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-02 15:06:14 +0100

    added square;

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.createElement("div")
+document.body.appendChild(container)
+const div = document.querySelector("div")
+let divX = 150;
+let divY = 50;
+div.style.left = `${divX}px`;
+div.style.top =  `${divY}px`;
+
+div.addEventListener("mousedown", () => {
+
+})
+div.addEventListener("mousemove", () => {
+    
+})
+div.addEventListener("mouseup", () => {
+    
+})
