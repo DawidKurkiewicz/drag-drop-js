69fcf1ca07dfb5ea5e0ba0d72ea60ec2e2875008
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-02 15:13:10 +0100

    added drag and drop

--- script.js
+++ script.js
@@ -4,14 +4,27 @@ const div = document.querySelector("div")
 let divX = 150;
 let divY = 50;
 div.style.left = `${divX}px`;
-div.style.top =  `${divY}px`;
+div.style.top = `${divY}px`;
+
+let drawActive = false
 
 div.addEventListener("mousedown", () => {
+    div.style.backgroundColor = "grey"
+    drawActive = true
+    insertDivX = e.offsetX
+    insertDivY = e.offsetY
+
 
 })
-div.addEventListener("mousemove", () => {
-    
+div.addEventListener("mousemove", (e) => {
+    if(drawActive) {
+    divX = e.clientX
+    divY = e.clientY
+    div.style.left = `${divX - 50}px`;
+    div.style.top = `${divY - 50}px`;
+}
 })
 div.addEventListener("mouseup", () => {
-    
+    div.style.backgroundColor = "black"
+    drawActive = false
 })
